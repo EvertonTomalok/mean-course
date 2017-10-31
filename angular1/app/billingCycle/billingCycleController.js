2b0e89a1a3c2ad633c41bbce4ee4930a04712732
Everton Tomalok <evertontomalok123@gmail.com>
2017-10-31 21:37:30 -0200

    começando implementar 6 botoes debts and credits

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -11,7 +11,7 @@
 
     self.refresh = function() {
       $http.get(url).then(function(response) {
-        self.billingCycle = {}
+        self.billingCycle = {credits:[{}], debts:[{}]}
         self.billingCycles = response
         tabs.show(self, { tabList: true, tabCreate: true}) // self sera o owner do método show TabDelete e Update recebem falso por padrao, e nao aparecem no ng-if
       })
@@ -19,12 +19,42 @@
 
     self.create = function() {
       $http.post(url, self.billingCycle).then(function(response){
-        msgs.addSuccess(`Operação realizada com sucesso!` )
         self.refresh()
+        msgs.addSuccess(`Operação realizada com sucesso!` )
       }).catch(function(resp) {
         msgs.addError(resp.data.errors)
       })
     }
+
+    self.showTabUpdate = function(billingCycle) {
+      self.billingCycle = billingCycle
+      tabs.show(self, {tabUpdate: true})
+    }
+
+    self.showTabDelete = function (billingCycle) {
+      self.billingCycle = billingCycle
+      tabs.show(self, {tabDelete: true})
+    }
+
+    self.delete = function(billingCycle) {
+      $http.delete(`${url}/${billingCycle._id}`, self.billingCycle).then(function(response) {
+        msgs.addSuccess(`Excluido com sucesso!`)
+      }).catch(function(resp) {
+        msgs.addError(resp.data.errors)
+      })
+    }
+
+    self.update = function (billingCycle) {
+      self.billingCycle = billingCycle
+      $http.put(`${url}/${billingCycle._id}`, self.billingCycle).then(function() {
+        self.refresh()
+      }).then(function() { // posso colocar um then atras do outro, quando for sucesso
+        msgs.addSuccess(`Alterado com sucesso!`)
+      }).catch(function(resp) {
+        msgs.addError(resp.data.errors)
+      })
+    }
+
     self.refresh()
   }
 })()
--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,34 @@
 const mongoose = require('mongoose')
 
-module.exports = mongoose.connect('mongodb://localhost/db_finance')
+const DbURL = 'mongodb://localhost/db_finance'
+
+module.exports = mongoose.connect(DbURL)
+
+//config monitoramento
+mongoose.connection.on('connected', function () {
+  console.log(`Mongoose rodando em: ${DbURL}`);
+})
+
+mongoose.connection.on('error', function (err) {
+  console.log(`Mongoose erro: `, err);
+})
+
+mongoose.connection.on('disconnected', function () {
+  console.log(`Mongoose desconectado de: ${DbURL}`);
+})
+
+mongoose.connection.on('open', function () {
+  console.log(`Mongoose conexão aberta`);
+})
+
+process.on('SIGINT', function () {
+  mongoose.connection.close(function () {
+    console.log(`Mongoose desconectado de: ${DbURL}`);
+    process.exit(0)
+  })
+})
+// fim config monitoramento
+
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = "O valor '{VALUE}' informado é menor que o limite minimo de '{MIN}'."
