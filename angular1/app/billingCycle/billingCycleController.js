ddb6eeb346e4e252243658068aa20bfb1f283d71
Everton Tomalok <evertontomalok123@gmail.com>
2017-10-30 22:22:09 -0200

    Controller arrumado

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -1,49 +1,22 @@
-// (function() {
-//   angular.module('primeiraApp').controller('BillingCycleCtrl', [
-//     '$http',
-//     BillingCycleController
-//   ])
-//   const self = this
-//   const url = `http://localhost:3003/api/billingCycles`
-//
-//   // function BillingCycleController() {
-//   //
-//   //   self.create = function () {
-//   //     const url = 'http://localhost:3003/api/billingCycles'
-//   //     $http.post(url, self.billingCycle).success(function(response) {
-//   //       self.billingCycle = {}
-//   //       console.log('Sucesso!')
-//   //     })
-//   //   }
-//   // }
-//
-//   function BillingCycleController($http) {
-//     self.create = function() {
-//       $http.post(url, self.billingCycle).then(function(response) {
-//         console.log("Sucesso!");
-//       })
-//     }
-//   }
-//
-//   BillingCycleController()
-// })()
-(function() {
-  angular.module('primeiraApp').controller('BillingCycleCtrl', [
-    '$scope',
-    '$http',
-    'msgs',
-    BillingCycleController,
-  ])
-    function BillingCycleController($scope, $http, msgs){
+(function(){
+    angular.module('primeiraApp').controller('BillingCycleCtrl', [
+        '$http',
+        'msgs',
+        BillingCycleController
+    ])
+    function BillingCycleController($http, msgs) {
+        const vm = this
+        const url = `http://localhost:3003/api/billingCycles`
+        
+        vm.create = function() {
+          console.log('1');
 
-    $scope.create = function() {
-      const url = `http://localhost:3003/api/billingCycles`;
-      $http.post(url, $scope.billingCycle).then(function(response) {
-        $scope.billingCycle = {}
-        msgs.addSuccess('Operação realizada com sucesso!!')
-      }).catch(function(resp) {
-        msgs.addError(resp.data.errors)
-      })
-      }
+            $http.post(url, vm.billingCycle).then(function(response){
+                vm.billingCycle = {}
+                msgs.addSuccess('sucesso!')
+            }).catch(function(resp) {
+                msgs.addError(resp.data.errors)
+                })
+        }
     }
 })()
